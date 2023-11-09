a0b74eb55ee4a14bff0b2ba1f5a8822fb9915c3f
dgiaquinta92 <43120495+dgiaquinta92@users.noreply.github.com>
2023-11-09 20:50:36 +0100

    Update nuevas librerias

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-from typing import Optional
+from fastapi import FastAPI, Depends, Request, HTTPException
+from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse, JSONResponse, RedirectResponse
+from starlette.responses import JSONResponse, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.security import HTTPBasicCredentials
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI, HTTPException
+import pandas as pd
+import io
+import requests
+import urllib3
+urllib3.disable_warnings()
+
+from security import security
+from procedures import procedures
+from modelos import modelos as md
+from routers import router_v1
+from logs import logs_middleware
+
 
 app = FastAPI()
+
+
+tags_metadata = [
+    {
+        "name": "DRINKS",
+        "description": "Request de Obtención de CSV DRINKS",
+        "externalDocs": {
+            "description": "Link",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+    {
+        "name": "TEST",
+        "description": "Request de prueba",
+        "externalDocs": {
+            "description": "Link a Dashboard o algo",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    }    
+]
+description = "API de Prueba"
+
+
+
+app = FastAPI(title="API - PyDay2023", version="1.0", 
+              description = description, openapi_tags=tags_metadata)
+
+app.include_router(router_v1.router, prefix="/api/v1")
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], 
+                   allow_credentials=True, allow_methods=["*"], 
+                   allow_headers=["*"])
+app.add_middleware(logs_middleware.RequestLoggingMiddleware)
+
 
 
 @app.get("/")
@@ -14,24 +64,3 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-
-# Lista de clientes inversos permitidos
-allowed_clients = {
-    "client1": "192.168.1.2",
-    "client2": "192.168.1.3"
-}
-
-@app.post("/send_command/{client_id}")
-async def send_command_to_client(client_id: str, command: str):
-    if client_id not in allowed_clients:
-        raise HTTPException(status_code=403, detail="Cliente no permitido")
-    
-    client_ip = allowed_clients[client_id]
-    response = send_command_to_inverse_client(client_ip, command)
-    return response
-
-def send_command_to_inverse_client(client_ip, command):
-    # Aquí usar la biblioteca adecuada para enviar comandos a clientes inversos
-    # Por ejemplo, la librería 'socket' o 'httpx' (dependiendo de la implementación)
-    # y manejar la comunicación con el cliente inverso
-    pass
